3901842d968a573a3eae7952e7800817359acab2
Boris Modylevsky <borismod@gmail.com>
2016-06-29 13:18:09 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+###Base template for creating CloudShell shell
+
+For detailed documentation please refer to the documentation of [shellfoundry](https://github.com/QualiSystems/shellfoundry)
---
+++ {{cookiecutter.project_slug}}/src/driver.py
@@ -0,0 +1,19 @@
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+
+
+class {{cookiecutter.driver_name}} (ResourceDriverInterface):
+
+    def cleanup(self):
+        pass
+
+    def __init__(self):
+        """
+        ctor must be without arguments, it is created with reflection at run time
+        """
+        pass
+
+    def initialize(self, context):
+        pass
+
+    def method_one(self, context, request):
+        pass
