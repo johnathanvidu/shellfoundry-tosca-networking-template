42e990f6616fc321c914a3b63ca41307c874e263
Boris Modylevsky <borismod@gmail.com>
2016-09-18 10:03:56 +0300

    update driver with docstring

--- {{cookiecutter.project_slug}}/src/driver.py
+++ {{cookiecutter.project_slug}}/src/driver.py
@@ -1,4 +1,6 @@
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCommandContext, AutoLoadResource, \
+    AutoLoadAttribute, AutoLoadDetails, AutoLoadCommandContext
 
 
 class {{cookiecutter.driver_name}} (ResourceDriverInterface):
@@ -13,7 +15,35 @@ class {{cookiecutter.driver_name}} (ResourceDriverInterface):
         pass
 
     def initialize(self, context):
+        """
+        Called upon driver initialization
+        :type context: InitCommandContext
+        :return:
+        """
         pass
 
-    def method_one(self, context, request):
+    def get_inventory(self, context):
+        """
+        Queries the device and returns a list of sub-resources and attribute values to CloudShell
+        :type context: AutoLoadCommandContext
+        :rtype AutoLoadDetails
+        """
+        # run 'shellfoundry generate' in order to create classes that represent your data model
+        resource = {{cookiecutter.model_name}}.create_from_context(context)
+        resource.name = 'fill the name'
+
+        # Add sub resources if needed
+        # resource.add_sub_resource('1', sub resource)
+
+        return resource.create_autoload_details()
+
+    def method_one(self, context,):
+        """
+        A simple driver command
+        :type context: ResourceCommandContext
+        :param context: the context the command runs on
+        :return:
+        """
+        # run 'shellfoundry generate' in order to create classes that represent your data model
+        resource = {{cookiecutter.model_name}}.create_from_context(context)
         pass
